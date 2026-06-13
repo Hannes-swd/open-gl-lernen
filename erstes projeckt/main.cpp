43e87a5e83701cd20d06eb3ca71da854a22c4643
Hannes <hannes-swd@users.noreply.github.com>
2026-06-13 23:31:25 +0200

    3 dreiecke

--- erstes projeckt/main.cpp
+++ erstes projeckt/main.cpp
@@ -29,9 +29,19 @@ int main() {
 
     //alle punkte setzen
     GLfloat vertices[] = {
-        -0.5f, -0.5f * float(sqrt(3)) / 3, 0.0f,
-         0.5f, -0.5f * float(sqrt(3)) / 3, 0.0f,
-         0.0f,  0.5f * float(sqrt(3)) * 2 / 3, 0.0f
+        -0.5f,        -0.5f * float(sqrt(3)) / 3,      0.0f, // Lower left corner
+         0.5f,        -0.5f * float(sqrt(3)) / 3,      0.0f, // Lower right corner
+         0.0f,         0.5f * float(sqrt(3)) * 2 / 3,  0.0f, // Upper corner
+        -0.5f / 2,     0.5f * float(sqrt(3)) / 6,      0.0f, // Inner left
+         0.5f / 2,     0.5f * float(sqrt(3)) / 6,      0.0f, // Inner right
+         0.0f,        -0.5f * float(sqrt(3)) / 3,      0.0f  // Inner down
+    };
+
+    GLuint indices[] = 
+    {
+        0, 3, 5, //unten links
+        3, 2, 4, //unten rechts
+        5, 4, 1  //oben
     };
 
     //fenster mit gröse öfnen
@@ -72,9 +82,10 @@ int main() {
     glDeleteShader(fragmentShader);
 
 
-    GLuint VAO, VBO;
+    GLuint VAO, VBO, EBO;
     glGenVertexArrays(1, &VAO);
     glGenBuffers(1, &VBO);
+    glGenBuffers(1, &EBO);
 
     //setz lles zusammen
     glBindVertexArray(VAO);
@@ -82,11 +93,17 @@ int main() {
 
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
+
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
     glEnableVertexAttribArray(0);
 
     glBindBuffer(GL_ARRAY_BUFFER, 0);
     glBindVertexArray(0);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
 
 
     glClearColor(0.07f, 0.13f, 0.67f, 1.0f );
@@ -102,7 +119,7 @@ int main() {
         glUseProgram(shaderProgram);
         glBindVertexArray(VAO);
 
-        glDrawArrays(GL_TRIANGLES, 0, 3);
+        glDrawElements(GL_TRIANGLES, 9, GL_UNSIGNED_INT, 0);
 
         glfwSwapBuffers(window);
 
@@ -112,6 +129,7 @@ int main() {
     //loscht alles was es nicht mer braucht
     glDeleteVertexArrays(1, &VAO);
     glDeleteBuffers(1, &VBO);
+    glDeleteBuffers(1, &EBO);
     glDeleteProgram(shaderProgram);
 
     //schliest das fenster
