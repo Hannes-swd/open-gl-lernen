9e2b2cbee1e9bab6563c8a1eb9f0568ee9543d92
Hannes <hannes-swd@users.noreply.github.com>
2026-06-16 00:37:35 +0200

    Texturen

--- erstes projeckt/src/main.cpp
+++ erstes projeckt/src/main.cpp
@@ -5,6 +5,8 @@
 #include <iostream>
 #include <string>
 
+#include <stb/stb_image.h>
+
 #include "shaderClass.h"
 
 #include "VAO.h"
@@ -32,22 +34,21 @@ int main() {
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-    //alle punkte setzen
-    GLfloat vertices[] = {
-        //            Cordinates                                farben
-        -0.5f,        -0.5f * float(sqrt(3)) / 3,      0.0f,    0.8f, 0.3f, 1.00f,  // Lower left corner
-         0.5f,        -0.5f * float(sqrt(3)) / 3,      0.0f,    0.0f, 0.0f, 0.22f,  // Lower right corner
-         0.0f,         0.5f * float(sqrt(3)) * 2 / 3,  0.0f,    0.3f, 0.3f, 0.32f,  // Upper corner
-        -0.5f / 2,     0.5f * float(sqrt(3)) / 6,      0.0f,    0.5f, 0.2f, 0.52f,  // Inner left
-         0.5f / 2,     0.5f * float(sqrt(3)) / 6,      0.0f,    0.6f, 0.3f, 0.02f,  // Inner right
-         0.0f,        -0.5f * float(sqrt(3)) / 3,      0.0f,    1.0f, 0.5f, 0.22f   // Inner down
+    // Vertices coordinates
+    GLfloat vertices[] =
+    {
+        //   COORDINATES           /      COLORS      //
+        -0.5f, -0.5f, 0.0f,    1.0f, 0.0f, 0.0f,    0.0f, 0.0f,  // Lower left corner
+        -0.5f,  0.5f, 0.0f,    0.0f, 1.0f, 0.0f,    0.0f, 1.0f,  // Upper left corner
+         0.5f,  0.5f, 0.0f,    0.0f, 0.0f, 1.0f,    1.0f, 1.0f,  // Upper right corner
+         0.5f, -0.5f, 0.0f,    1.0f, 1.0f, 1.0f,    1.0f, 0.0f,  // Lower right corner
     };
 
+    // Indices for vertices order
     GLuint indices[] =
     {
-        0, 3, 5, //unten links
-        3, 2, 4, //unten rechts
-        5, 4, 1  //oben
+        0, 2, 1, // Upper triangle
+        0, 3, 2  // Lower triangle
     };
 
     //fenster mit gröse öfnen
@@ -81,24 +82,54 @@ int main() {
     VBO vbo(vertices, sizeof(vertices));
     EBO ebo((GLfloat*)indices, sizeof(indices));
 
-    vao.LinkAttrib(vbo, 0, 3, GL_FLOAT, 6 * sizeof(GLfloat), (void*)0);
-    vao.LinkAttrib(vbo, 1, 3, GL_FLOAT, 6 * sizeof(GLfloat), (void*)(3 * sizeof(GLfloat)));
+    vao.LinkAttrib(vbo, 0, 3, GL_FLOAT, 8 * sizeof(GLfloat), (void*)0);
+    vao.LinkAttrib(vbo, 1, 3, GL_FLOAT, 8 * sizeof(GLfloat), (void*)(3 * sizeof(GLfloat)));
+    vao.LinkAttrib(vbo, 2, 2, GL_FLOAT, 8 * sizeof(GLfloat), (void*)(6 * sizeof(GLfloat)));
 
     vao.Unbind();
     vbo.Unbind();
     ebo.Unbind();
 
     GLuint uniID = glGetUniformLocation(shaderProgram.ID, "scale");
-    
+
+    //texture
+    int widthImg, heightImg, numColCh;
+    stbi_set_flip_vertically_on_load(true);
+    unsigned char* bytes = stbi_load((base + "/res/textures/pop_cat.png").c_str(), &widthImg, &heightImg, &numColCh, 0);
+
+    GLuint texture;
+    glGenTextures(1, &texture);
+    glActiveTexture(GL_TEXTURE0);
+    glBindTexture(GL_TEXTURE_2D, texture);
+
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, widthImg, heightImg, 0, GL_RGBA, GL_UNSIGNED_BYTE, bytes);
+
+    glGenerateMipmap(GL_TEXTURE_2D);
+
+    stbi_image_free(bytes);
+    glBindTexture(GL_TEXTURE_2D, 0);
+
+
+    GLuint tex0Uni = glGetUniformLocation(shaderProgram.ID, "tex0");
+    shaderProgram.Activate();
+    glUniform1i(tex0Uni, 0);
+
     //mainloop
     while (!glfwWindowShouldClose(window)) {
         glClearColor(0.07f, 0.13f, 0.67f, 1.0f );
         glClear(GL_COLOR_BUFFER_BIT);
         shaderProgram.Activate();
         glUniform1f(uniID, 0.5f);
+        glBindTexture(GL_TEXTURE_2D, texture);
         vao.Bind();
 
-        glDrawElements(GL_TRIANGLES, 9, GL_UNSIGNED_INT, 0);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 
         glfwSwapBuffers(window);
 
@@ -109,6 +140,7 @@ int main() {
     vao.Delete();
     vbo.Delete();
     ebo.Delete();
+    glDeleteTextures(1, &texture);
     shaderProgram.Delete();
 
     //schliest das fenster
@@ -117,5 +149,5 @@ int main() {
 }
 
 
-// https://youtu.be/45MIykWJ-C4?si=XxRzZXBJwL-4rtaF&t=2794
+// https://youtu.be/45MIykWJ-C4?si=A3isLb6ZQDmtQtHv&t=3336
 // cmake --build "C:\Users\hanne\Cpp\cpp open gl lernen\erstes projeckt\build" --config Release; & "C:\Users\hanne\Cpp\cpp open gl lernen\erstes projeckt\build\Release\ErstesProjekt.exe"
